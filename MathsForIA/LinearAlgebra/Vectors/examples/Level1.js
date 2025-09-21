16baec94c3366b9f5469109ecdbb53cdfa1d7df0
br7forlife <diazpachecobrayan@gmail.com>
2025-09-20 21:09:58 -0500

    add one exercise for veridict functionality for module vector

---
+++ MathsForIA/LinearAlgebra/Vectors/examples/Level1.js
@@ -0,0 +1,10 @@
+/** calcula la suma de los siguientes vectores
+ * 
+ *  [1,2,3][4,5,6][7,8,9]
+ * 
+ * **/
+
+import operationWithVectors from "../Vector.js";
+
+console.log(operationWithVectors("distanceVector", [[1,2,3], [4,5,6], [7,8,9]],3))
+
